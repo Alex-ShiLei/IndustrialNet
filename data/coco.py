4dabde6455735b7fdf3d57bc6dd8f69c94aef31c
Alex-ShiLei <2457072341@qq.com>
2022-12-14 17:20:50 +0800

    Create coco.py

---
+++ data/coco.py
@@ -0,0 +1,305 @@
+r""" COCO-20i few-shot semantic segmentation dataset """
+import os
+import pickle
+
+from torch.utils.data import Dataset
+import torch.nn.functional as F
+import torch
+import PIL.Image as Image
+#from pycocotools.coco import orgCOCO
+import json
+import time
+import numpy as np
+import itertools
+from collections import defaultdict
+import sys
+import cv2
+PYTHON_VERSION = sys.version_info[0]
+if PYTHON_VERSION == 2:
+    from urllib import urlretrieve
+elif PYTHON_VERSION == 3:
+    from urllib.request import urlretrieve
+
+
+def _isArrayLike(obj):
+    return hasattr(obj, '__iter__') and hasattr(obj, '__len__')
+
+
+class COCO:
+    def __init__(self, annotation_file=None):
+        """
+        Constructor of Microsoft COCO helper class for reading and visualizing annotations.
+        :param annotation_file (str): location of annotation file
+        :param image_folder (str): location to the folder that hosts images.
+        :return:
+        """
+        # load dataset
+        self.dataset,self.anns,self.cats,self.imgs,self.imgNameId,self.clsId = dict(),dict(),dict(),dict(),dict(),dict()
+        self.imgToAnns, self.catToImgs = defaultdict(list), defaultdict(list)
+        if not annotation_file == None:
+            print('loading annotations into memory...')
+            tic = time.time()
+            dataset = json.load(open(annotation_file, 'r'))
+            assert type(dataset)==dict, 'annotation file format {} not supported'.format(type(dataset))
+            print('Done (t={:0.2f}s)'.format(time.time()- tic))
+            self.dataset = dataset
+            self.createIndex()
+            self.idToCId()
+    def idToCId(self):
+        for idx, val in enumerate(self.dataset['categories']):
+            self.clsId[idx]=val['id']
+    def createIndex(self):
+        # create index
+        print('creating index...')
+        anns, cats, imgs = {}, {}, {}
+        imgToAnns,catToImgs = defaultdict(list),defaultdict(list)
+        if 'annotations' in self.dataset:
+            for ann in self.dataset['annotations']:
+                imgToAnns[ann['image_id']].append(ann)
+                anns[ann['id']] = ann
+
+        if 'images' in self.dataset:
+            for img in self.dataset['images']:
+                imgs[img['id']] = img
+        if 'images' in self.dataset:
+            for img in self.dataset['images']:
+                self.imgNameId[img['file_name']] = img
+
+        if 'categories' in self.dataset:
+            for cat in self.dataset['categories']:
+                cats[cat['id']] = cat
+
+        if 'annotations' in self.dataset and 'categories' in self.dataset:
+            for ann in self.dataset['annotations']:
+                catToImgs[ann['category_id']].append(ann['image_id'])
+
+        print('index created!')
+
+        # create class members
+        self.anns = anns
+        self.imgToAnns = imgToAnns
+        self.catToImgs = catToImgs
+        self.imgs = imgs
+        self.cats = cats
+
+    def info(self):
+        """
+        Print information about the annotation file.
+        :return:
+        """
+        for key, value in self.dataset['info'].items():
+            print('{}: {}'.format(key, value))
+
+    def getAnnIds(self, imgIds=[], catIds=[], areaRng=[], iscrowd=None):
+        """
+        Get ann ids that satisfy given filter conditions. default skips that filter
+        :param imgIds  (int array)     : get anns for given imgs
+               catIds  (int array)     : get anns for given cats
+               areaRng (float array)   : get anns for given area range (e.g. [0 inf])
+               iscrowd (boolean)       : get anns for given crowd label (False or True)
+        :return: ids (int array)       : integer array of ann ids
+        """
+        imgIds = imgIds if _isArrayLike(imgIds) else [imgIds]
+        catIds = catIds if _isArrayLike(catIds) else [catIds]
+
+        if len(imgIds) == len(catIds) == len(areaRng) == 0:
+            anns = self.dataset['annotations']
+        else:
+            if not len(imgIds) == 0:
+                lists = [self.imgToAnns[imgId] for imgId in imgIds if imgId in self.imgToAnns]
+                anns = list(itertools.chain.from_iterable(lists))
+            else:
+                anns = self.dataset['annotations']
+            anns = anns if len(catIds)  == 0 else [ann for ann in anns if ann['category_id'] in catIds]
+            anns = anns if len(areaRng) == 0 else [ann for ann in anns if ann['area'] > areaRng[0] and ann['area'] < areaRng[1]]
+        if not iscrowd == None:
+            ids = [ann['id'] for ann in anns if ann['iscrowd'] == iscrowd]
+        else:
+            ids = [ann['id'] for ann in anns]
+        return ids
+
+    def getCatIds(self, catNms=[], supNms=[], catIds=[]):
+        """
+        filtering parameters. default skips that filter.
+        :param catNms (str array)  : get cats for given cat names
+        :param supNms (str array)  : get cats for given supercategory names
+        :param catIds (int array)  : get cats for given cat ids
+        :return: ids (int array)   : integer array of cat ids
+        """
+        catNms = catNms if _isArrayLike(catNms) else [catNms]
+        supNms = supNms if _isArrayLike(supNms) else [supNms]
+        catIds = catIds if _isArrayLike(catIds) else [catIds]
+
+        if len(catNms) == len(supNms) == len(catIds) == 0:
+            cats = self.dataset['categories']
+        else:
+            cats = self.dataset['categories']
+            cats = cats if len(catNms) == 0 else [cat for cat in cats if cat['name']          in catNms]
+            cats = cats if len(supNms) == 0 else [cat for cat in cats if cat['supercategory'] in supNms]
+            cats = cats if len(catIds) == 0 else [cat for cat in cats if cat['id']            in catIds]
+        ids = [cat['id'] for cat in cats]
+        return ids
+
+    def getImgIds(self, imgIds=[], catIds=[]):
+        '''
+        Get img ids that satisfy given filter conditions.
+        :param imgIds (int array) : get imgs for given ids
+        :param catIds (int array) : get imgs with all given cats
+        :return: ids (int array)  : integer array of img ids
+        '''
+        imgIds = imgIds if _isArrayLike(imgIds) else [imgIds]
+        catIds = catIds if _isArrayLike(catIds) else [catIds]
+
+        if len(imgIds) == len(catIds) == 0:
+            ids = self.imgs.keys()
+        else:
+            ids = set(imgIds)
+            for i, catId in enumerate(catIds):
+                if i == 0 and len(ids) == 0:
+                    ids = set(self.catToImgs[catId])
+                else:
+                    ids &= set(self.catToImgs[catId])
+        return list(ids)
+
+    def loadAnns(self, ids=[]):
+        """
+        Load anns with the specified ids.
+        :param ids (int array)       : integer ids specifying anns
+        :return: anns (object array) : loaded ann objects
+        """
+        if _isArrayLike(ids):
+            return [self.anns[id] for id in ids]
+        elif type(ids) == int:
+            return [self.anns[ids]]
+
+    def loadCats(self, ids=[]):
+        """
+        Load cats with the specified ids.
+        :param ids (int array)       : integer ids specifying cats
+        :return: cats (object array) : loaded cat objects
+        """
+        if _isArrayLike(ids):
+            return [self.cats[id] for id in ids]
+        elif type(ids) == int:
+            return [self.cats[ids]]
+
+    def loadImgs(self, ids=[]):
+        """
+        Load anns with the specified ids.
+        :param ids (int array)       : integer ids specifying img
+        :return: imgs (object array) : loaded img objects
+        """
+        if _isArrayLike(ids):
+            return [self.imgs[id] for id in ids]
+        elif type(ids) == int:
+            return [self.imgs[ids]]
+
+class DatasetCOCO(Dataset):
+    def __init__(self, datapath, fold, transform, split, shot, use_original_imgsize):
+        self.split = 'val' if split in ['val', 'test'] else 'trn'
+        self.fold = fold
+        self.nfolds = 4
+        self.nclass = 80
+        self.benchmark = 'coco'
+        self.shot = shot
+        self.split_coco = 'val2014' if self.split == 'val' else 'train2014'
+        self.base_path = os.path.join(datapath, 'COCO2014')
+        self.annotion_path=self.base_path+'/annotations/instances_'+self.split_coco+'.json'
+        print('load annotions in:',self.annotion_path,self.split)
+        self.coco=COCO(self.annotion_path)
+        self.transform = transform
+        self.use_original_imgsize = use_original_imgsize
+
+        self.class_ids = self.build_class_ids()
+        self.img_metadata_classwise = self.build_img_metadata_classwise()
+        self.img_metadata = self.build_img_metadata()
+
+    def __len__(self):
+        return 7500 if self.split == 'trn' else 1000
+
+    def __getitem__(self, idx):
+        # ignores idx during training & testing and perform uniform sampling over object classes to form an episode
+        # (due to the large size of the COCO dataset)
+        query_img, query_mask, support_imgs, support_masks, query_name, support_names, class_sample, org_qry_imsize = self.load_frame()
+
+        query_img = self.transform(query_img)
+        query_mask = query_mask.float()
+        if not self.use_original_imgsize:
+            query_mask = F.interpolate(query_mask.unsqueeze(0).unsqueeze(0).float(), query_img.size()[-2:], mode='nearest').squeeze()
+
+        support_imgs = torch.stack([self.transform(support_img) for support_img in support_imgs])
+        for midx, smask in enumerate(support_masks):
+            support_masks[midx] = F.interpolate(smask.unsqueeze(0).unsqueeze(0).float(), support_imgs.size()[-2:], mode='nearest').squeeze()
+        support_masks = torch.stack(support_masks)
+
+        batch = {'query_img': query_img,
+                 'query_mask': query_mask,
+                 'query_name': query_name,
+
+                 'org_query_imsize': org_qry_imsize,
+
+                 'support_imgs': support_imgs,
+                 'support_masks': support_masks,
+                 'support_names': support_names,
+                 'class_id': torch.tensor(class_sample)}
+
+        return batch
+
+    def build_class_ids(self):
+        nclass_trn = self.nclass // self.nfolds
+        class_ids_val = [self.fold + self.nfolds * v for v in range(nclass_trn)]
+        class_ids_trn = [x for x in range(self.nclass) if x not in class_ids_val]
+        class_ids = class_ids_trn if self.split == 'trn' else class_ids_val
+
+        return class_ids
+
+    def build_img_metadata_classwise(self):
+        with open('./data/splits/coco/%s/fold%d.pkl' % (self.split, self.fold), 'rb') as f:
+            img_metadata_classwise = pickle.load(f)
+        return img_metadata_classwise
+
+    def build_img_metadata(self):
+        img_metadata = []
+        for k in self.img_metadata_classwise.keys():
+            img_metadata += self.img_metadata_classwise[k]
+        return sorted(list(set(img_metadata)))
+
+    def read_mask(self, name,class_id):
+        calssSeg=[]
+        imgInfo = self.coco.imgNameId[name]
+        h = imgInfo['height']
+        w = imgInfo['width']
+        gt = np.zeros((h, w), dtype=np.uint8)
+        imgid = self.coco.getAnnIds(imgInfo['id'])
+        for id in imgid:
+            annotion = self.coco.anns[id]
+            if annotion['category_id']==self.coco.clsId[class_id]:
+                if annotion['iscrowd']:
+                    continue
+                for val in annotion['segmentation']:
+                    seg = np.array(val).reshape(-1, 2)
+                    calssSeg.append(seg.astype(np.int32)[np.newaxis, :, :])
+        cv2.fillPoly(gt, calssSeg, int(1))
+        return torch.tensor(gt)
+
+    def load_frame(self):
+        class_sample = np.random.choice(self.class_ids, 1, replace=False)[0]
+        query_name = np.random.choice(self.img_metadata_classwise[class_sample], 1, replace=False)[0]
+        query_img = Image.open(os.path.join(self.base_path, query_name)).convert('RGB')
+        query_mask = self.read_mask(query_name.split('/')[-1],class_sample)
+
+        org_qry_imsize = query_img.size
+        support_names = []
+        while True:  # keep sampling support set if query == support
+            support_name = np.random.choice(self.img_metadata_classwise[class_sample], 1, replace=False)[0]
+            if query_name != support_name: support_names.append(support_name)
+            if len(support_names) == self.shot: break
+        support_imgs = []
+        support_masks = []
+        for support_name in support_names:
+            support_imgs.append(Image.open(os.path.join(self.base_path, support_name)).convert('RGB'))
+            support_mask = self.read_mask(support_name.split('/')[-1],class_sample)
+            support_masks.append(support_mask)
+
+        return query_img, query_mask, support_imgs, support_masks, query_name, support_names, class_sample, org_qry_imsize
+
